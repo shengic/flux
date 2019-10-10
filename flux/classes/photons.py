30f939256ee6f0c45d08090cc0ef43121a9cb093
WS <>
2019-10-10 12:58:48 -0700

    debug stuffs

--- flux/classes/photons.py
+++ flux/classes/photons.py
@@ -7,13 +7,13 @@ from classes.tender import tender
 import scipy.constants as constants
 import scipy.special as special
 
-class photons(object):
+class Photons(object):
     def __init__(self, undulator, maxeV):
         self.undulator = undulator
 #reflectivity/absorption percentage list
-        self.ntotal = int(maxeV/(this.undulator.eV1))+1
+        self.ntotal = int(maxeV/(self.undulator.eV1))+1
 #create evlist per n
-        self.evList = np.arange(1, self.ntotal, 2) * self.undulator.ev1
+        self.evList = np.arange(1, self.ntotal, 2) * self.undulator.eV1
         self.evHash = {key : value for key, value in zip(np.arange(1, self.ntotal, 2), self.evList)}
         self.refList = np.repeat(1., len(self.evList))
         self.refHash = {key : value for key, value in zip(np.arange(1, self.ntotal, 2), self.refList)}
@@ -30,20 +30,23 @@ class photons(object):
         self.thetay = thetay
 
         self.photonFlux = np.power(constants.e*self.undulator.gamma*self.undulator.N,2)/constants.e* \
-            2.*math.pi/constants.Planck/(4.*constants.pi*constants.epepsilon_0*constants.c)
-        self.photonFlux *= self.undulator.ev1*1000./self.undulator.N*np.sum(self.LfunList())
+            2.*math.pi/constants.Planck/(4.*constants.pi*constants.epsilon_0*constants.c)
+        self.photonFlux *= self.undulator.eV1*1000./self.undulator.N*np.sum(self.LfunList)
+        return self.photonFlux
 
 
     def getLfunList(self):
-        self.LfunList = np.array()
+        self.LfunList = np.array([])
 # make sure each element is only odd number energy
         for n in np.arange(1, self.ntotal, 2):
-            self.LfunList.append(Fn(n)*n*self.refHash(n))
+            re = self.refHash[n]
+            t = self.Fn(n)*n*re
+            np.append(self.LfunList, t)
         return self.LfunList
 
     def Fn(self,n):
-        k = self.undulator.ky
-        z = n*np.power(kaiser,2)/(2.*(1.+ np.power(kaiser,2)/2))
-        self.Fn = np.power(n*k,2)/np.power((1.+ np.power(k,2)/2.) ,2) * \
+        k = self.undulator.Ky
+        z = n*np.power(k,2)/(2.*(1.+ np.power(k,2)/2))
+        fn = np.power(n*k,2)/np.power((1.+ np.power(k,2)/2.) ,2) * \
                    np.power(special.jv((n+1)/2,z)-special.jv((n-1)/2,z),2)
-        return self.Fn
+        return fn
